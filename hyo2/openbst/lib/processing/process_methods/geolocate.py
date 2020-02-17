d6ae548f7c0f898043e31196fe292b838e16c966
Michael <zxcvbnmike15@gmail.com>
2020-02-17 16:12:52 -0500

    WIP - geolocate methods
    Switching branches, saving progress on the current branch

--- hyo2/openbst/lib/processing/process_methods/geolocate.py
+++ hyo2/openbst/lib/processing/process_methods/geolocate.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from enum import Enum
 from netCDF4 import Dataset, Group
+from typing import Optional
 
 from hyo2.openbst.lib.nc_helper import NetCDFHelper
 
@@ -11,14 +12,12 @@ logger = logging.getLogger(__name__)
 
 # ## Sounding Position Enum and Dictionaries ##
 class GeolocateEnum(Enum):
-    slant_range = 0
-    raytrace = 1
-    imported_bathy = 2
+    compute = 0
+    imported_bathy = 1
 
 
-position_title = {
-    GeolocateEnum.slant_range: "Sounding Position - Slant Range Approximation",
-    GeolocateEnum.raytrace: "Sounding Position - Raytraced",
+geolocate_title = {
+    GeolocateEnum.compute: "Sounding Position - Computed",
     GeolocateEnum.imported_bathy: "Sounding Position - Imported"
 }
 
@@ -30,12 +29,27 @@ class GeolocationParams:
     def __init__(self):
         self.method_type = GeolocateEnum.slant_range
         self.motion_comp = False
+        self.raytrace = False
 
     def nc_write_parameters(self, grp_process: Group):
-        pass
+        try:
+            grp_process.title = geolocate_title[self.method_type]
+            grp_process.method_type = self.method_type.name
+            grp_process.motion_comp = str(self.motion_comp)
+            grp_process.raytrace = str(self.raytrace)
+            return True
+
+        except TypeError:
+            return False
 
     def process_identifiers(self) -> list:
-        pass
+        process_string = GeolocationParams.process_name
+        parameter_string = str()
+        for key, value in self.__dict__.items():
+            parameter_string += key + str(value)
+        parameter_hash = NetCDFHelper.hash_string(input_str=parameter_string)
+        process_ids = [process_string, parameter_hash]
+        return process_ids
 
 
 # ## Positioning Class and methods ##
@@ -47,7 +61,17 @@ class Geolocation:
     @classmethod
     def position_soundings(cls, ds_process: Dataset, ds_raw: Dataset,
                            parent: str, parameters: GeolocationParams):
-        pass
+        p_method_type = parameters.method_type
+        p_motion_comp = parameters.motion_comp
+
+        if p_method_type is GeolocateEnum.compute:
+            data_out = Geolocation.slant_range()
+
+        elif p_method_type is GeolocateEnum.imported_bathy:
+            data_out = Geolocation.import_bathy()
+        else:
+            raise TypeError("Unrecognized Geolocation Method: %s" % p_method_type)
+        return data_out
 
     @classmethod
     def write_data_to_nc(cls):
@@ -55,12 +79,25 @@ class Geolocation:
 
     # # Processing Methods Types:
     @classmethod
-    def slant_range(cls):
-        pass
+    def compute(cls,
+                sample_rate: np.ma.MaskedArray,
+                sensor_sound_speed: Optional[np.ma.MaskedArray],
+                detection_point: np.ma.MaskedArray,
+                lat: np.ma.MaskedArray,
+                lon: np.ma.MaskedArray,
+                roll: Optional[np.ma.MaskedArray],
+                pitch: Optional[np.ma.MaskedArray],
+                yaw: Optional[np.ma.MaskedArray]):
 
-    @classmethod
-    def raytrace(cls):
-        pass
+        # Calculate the range
+        if sensor_sound_speed is None:
+                sensor_sound_speed = 1500   # A static value of 1500m/s
+
+
+        range_m = detection_point / sample_rate * sensor_sound_speed / 2
+
+
+
 
     @classmethod
     def import_bathy(cls):
